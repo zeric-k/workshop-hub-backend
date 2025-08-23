ed2f08e4b93d6955d1a103ff5f14c58e51589194
zeric-k <kshitiz.sriv@gmail.com>
2025-08-23 23:53:34 +0530

    CORS fix

--- src/main/java/com/workshop/configuration/WebConfig.java
+++ src/main/java/com/workshop/configuration/WebConfig.java
@@ -1,7 +1,5 @@
 package com.workshop.configuration;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -18,7 +16,7 @@ public class WebConfig {
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
